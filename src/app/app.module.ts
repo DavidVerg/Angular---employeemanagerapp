0352585f129c0dea3a0befb63d41d5b404eba0f5
David Vergara <david.vergara82141@gmail.com>
2021-10-28 14:48:39 -0500

    Añadido proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { EmployeeService } from './services/employee.service';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+  ],
+  providers: [EmployeeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
